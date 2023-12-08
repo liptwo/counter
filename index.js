8b810fa4d1d85967e19f1e1df391017e623d742d
liptwo <liptwo0@gmail.com>
2023-12-08 10:45:26 +0700

    moi

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const numberCounter =  document.getElementById("sodem");
 const deBt = document.getElementById("button-de");
 const reBt = document.getElementById("button-re");
 const inBt = document.getElementById("button-in");
+const qrBt = document.getElementById("button-qr");
+const demo = document.querySelectorAll('.button-50')
 let counter=0;
 
 function updates(){
@@ -9,8 +11,21 @@ function updates(){
         counter =0;
     }
     numberCounter.innerHTML=counter;
-   
+} 
+qrBt.addEventListener("click", ()=>{
+        deBt.classList.toggle("button-50");
+        deBt.classList.toggle("button-54");
+
+        inBt.classList.toggle("button-50");
+        inBt.classList.toggle("button-54");
+
+        qrBt.classList.toggle("button-50");
+        qrBt.classList.toggle("button-54");
+
+        reBt.classList.toggle("button-50");
+        reBt.classList.toggle("button-54");
 }
+);
 inBt.addEventListener("click", ()=>{
     counter++;
     updates();
